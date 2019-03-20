b33a438357d8560b078749e9c2fdb0749d82b9c1
Kingsley Chukwu <chukwuk@flip1.engr.oregonstate.edu>
2019-03-20 13:19:17 -0700

    almost there

--- ta.cpp
+++ ta.cpp
@@ -32,11 +32,11 @@ using namespace std;
 //srand(time(0));
 
 /******************************************************
- ********************************** Function:Mazelevel
- ********************************** Description:it is a parameterized constructor that reads a text file containing the maze
- *********************************** Parameters: ifstream , two integers
- *********************************** Pre-Conditions: a readable file, two integers representing the maze height and width
- ********************************** Post-Conditions: reads  the maze into a vector of mazelocation
+ ********************************** Function:Ta
+ ********************************** Description:it is a parameterized constructor that initialize the locations of the two TAs
+ *********************************** Parameters: the mazelevel class, and two integers
+ *********************************** Pre-Conditions: the mazewidth; mazeheight; maze locations
+ ********************************** Post-Conditions: initialize the locations of the two TAs
  **************************************************************************************/
 
 
@@ -60,6 +60,17 @@ Ta::Ta(int rows, int cols, Mazelevel &m) {
 
 }
 
+
+/******************************************************
+ * ********************************** Function:get_move
+ * *********************************** Description: moves the two TAs randomly in the maze
+ * ************************************ Parameters:   the mazelevel class, and two integers
+ * *********************************** Pre-Conditions: the maze width; maze height; maze locations
+ * *********************************** Post-Conditions:  moves the two TAs randomly
+ * **************************************************************************************/
+
+
+
 void Ta::get_move(int rows, int cols, Mazelevel &m) {
      Mazelocation* locate;
 //    srand(time(0));
@@ -78,22 +89,71 @@ void Ta::get_move(int rows, int cols, Mazelevel &m) {
 
 }
 
+/******************************************************
+ * ********************************** Function:set_location
+ * *********************************** Description:sets the location of the first TA
+ * ************************************ Parameters: two  integers
+ * *********************************** Pre-Conditions: pass two integers representing the row and columns respectively
+ * *********************************** Post-Conditions:  sets the location of the first TA
+ * **************************************************************************************/
+
+
 
 void Ta::set_location(int x, int y) {
      this->rows = x;
      this->cols = y;
 }
 
+/******************************************************
+ * ********************************** Function:get_row
+ * *********************************** Description: used to return the row index of the TA
+ * ************************************ Parameters:  none
+ * *********************************** Pre-Conditions: none
+ * *********************************** Post-Conditions:  returns the row of the TA
+ * **************************************************************************************/
+
+
+
 int Ta::get_row() {
     return this->rows;
 }
+
+/******************************************************
+ * ********************************** Function:get_row
+ * *********************************** Description:used to return the column index of the TA
+ * ************************************ Parameters:  none
+ * *********************************** Pre-Conditions: none
+ * *********************************** Post-Conditions: return the column index of the TA 
+ * **************************************************************************************/
+
+
 int  Ta::get_col() {
     return this->cols;
 }
 
+/******************************************************
+ * ********************************** Function:get_row_1
+ * *********************************** Description:used to return the row index of the second TA
+ * ************************************ Parameters:  none
+ * *********************************** Pre-Conditions: none
+ * *********************************** Post-Conditions:  return the row index of the TA
+ * **************************************************************************************/
+
+
+
 int Ta::get_row_1() {
     return this->rows_1;
 }
+
+/******************************************************
+ * ********************************** Function:get_col_1
+ * *********************************** Description:used to return the column index of the second TA
+ * ************************************ Parameters:  none
+ * *********************************** Pre-Conditions: none
+ * *********************************** Post-Conditions:  return the column index of the TA
+ * **************************************************************************************/
+
+
 
 int  Ta::get_col_1() {
     return this->cols_1;
